19c64ba65ceb3cb8dd109a4be111ff19ba34e91d
Tapan Rathore <tapanrathore07@gmail.com>
2026-06-04 11:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code")
+codes[0].focus()
+
+codes.forEach((code, index)=>{
+
+    code.addEventListener("input", ()=>{
+        if(code.value !== "" &&  Number.isNaN(Number(code.value))){
+            code.value = "";
+        }
+
+        if(code.value && index < codes.length-1){
+            codes[index+1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e)=>{
+        // console.log(e.key)
+        if(e.key === "Backspace"){
+            if(code.value === "" && index>0){
+                codes[index-1].value="";
+                codes[index-1].focus();
+            }
+        }
+    })
+})
